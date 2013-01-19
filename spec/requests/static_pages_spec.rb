25146e92366aebec670bcb33c55ef30f5efbeb4e
Ranjay Krishna <rak248@cornell.edu>
2013-01-19 15:14:55 -0800

    links added. signup page added

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,58 +3,26 @@ require 'spec_helper'
 describe "Static pages" do
 
   describe "Home page" do
-
-    it "should have the h1 'Chirpr'" do
-      visit root_path
-      page.should have_selector('h1', :text => 'Welcome to Chirpr')
-    end
-
-    it "should have the title 'Home'" do
-      visit root_path
-      page.should have_selector('title',
-                        :text => "Chirpr | Home")
-    end
+    before { visit root_path }
+    it { page.should have_selector('h1', :text => 'Welcome to Chirpr') }
+    it { page.should have_selector('title', :text => "Chirpr | Home") }
   end
 
   describe "Help page" do
-
-    it "should have the h1 'Help'" do
-      visit help_path
-      page.should have_selector('h1', :text => 'Help')
-    end
-
-    it "should have the title 'Help'" do
-      visit help_path
-      page.should have_selector('title',
-                        :text => "Chirpr | Help")
-    end
+    before { visit help_path }
+    it { page.should have_selector('h1', :text => 'Help') }
+    it { page.should have_selector('title', :text => "Chirpr | Help") }
   end
 
   describe "About page" do
-
-    it "should have the h1 'About Us'" do
-      visit about_path
-      page.should have_selector('h1', :text => 'About Us')
-    end
-
-    it "should have the title 'About Us'" do
-      visit about_path
-      page.should have_selector('title',
-                    :text => "Chirpr | About Us")
-    end
+    before { visit about_path }
+    it { page.should have_selector('h1', :text => 'About Us') }
+    it { page.should have_selector('title', :text => "Chirpr | About Us") }
   end
   
   describe "Contact page" do
-
-    it "should have the h1 'Contact'" do
-      visit contact_path
-      page.should have_selector('h1', text: 'Contact')
-    end
-
-    it "should have the title 'Contact'" do
-      visit contact_path
-      page.should have_selector('title',
-                    text: "Chirpr | Contact")
-    end
+    before { visit contact_path }
+    it { page.should have_selector('h1', text: 'Contact') }
+    it { page.should have_selector('title', text: "Chirpr | Contact") }
   end
 end
